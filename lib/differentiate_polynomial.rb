fbae99c931f74f7bb297645abbe93617943cd036
Eg-ork-a <92421937+Eg-ork-a@users.noreply.github.com>
2023-05-24 11:00:17 +0300

    Fix differentiate_polynomial

--- lib/differentiate_polynomial.rb
+++ lib/differentiate_polynomial.rb
@@ -1,30 +1,27 @@
-require_relative "differentiate_monomial"
+require_relative 'differentiate_monomial'
 
 def differentiate_polynomial(input, differentiate_variable)
-  if input.match('\A(-?[^+-]+)([+-][^+-]+)*\z') == nil then return nil end
-  if differentiate_variable.match('\A[a-z]\z') == nil then return nil end
+  return nil if input.match('\A(-?[^+-]+)([+-][^+-]+)*\z').nil?
+  return nil if differentiate_variable.match('\A[a-z]\z').nil?
 
-  if input[0] == '-' then
+  if input[0] == '-'
     sign = '-'
     index_sign = 0
   else
     sign = '+'
     index_sign = -1
   end
-  
-  index=1
-  res=""
 
-  while index<input.length do
-    if input[index] == '+' || input[index] == '-' then 
-      temp = input[index_sign+1...index]
+  index = 1
+  res = ''
+
+  while index <= input.length
+    if (input[index] == '+' || input[index] == '-') || index == input.length
+      temp = input[index_sign + 1...index]
       monomial = differentiate_monomial(temp, differentiate_variable)
-      if monomial == nil then
-        print "'"
-        print temp
-        print "'\n"
+      if monomial.nil?
         return nil
-      elsif monomial != "" then
+      elsif monomial != ''
         res << (sign == '+' && index_sign == -1 ? '' : sign) + monomial
       end
       sign = input[index]
@@ -32,5 +29,9 @@ def differentiate_polynomial(input, differentiate_variable)
     end
     index += 1
   end
-  res
+  if res == ''
+    '0'
+  else
+    res
+  end
 end
