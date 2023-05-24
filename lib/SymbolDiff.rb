17f29fe67db74bd9f77ed5c71953fc946b74a4f3
ga12lil <92907053+ga12lil@users.noreply.github.com>
2023-05-24 11:03:57 +0300

    configurate project

--- lib/SymbolDiff.rb
+++ lib/SymbolDiff.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "SymbolDiff/version"
+require_relative "differentiate_polynomial"
 
 module SymbolDiff
   extend self
   class Error < StandardError; end
 
-  def hello
-    "hello"
+  def diff(input,differentiate_variable)
+    differentiate_polynomial(input,differentiate_variable)
   end
 end
